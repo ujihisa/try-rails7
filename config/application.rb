cde559240dff07c643be55c4e061ec17c0676afd
ujihisa <ujihisa@users.noreply.github.com>
2021-09-24 20:50:29 -0700

    Use sqlite3

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,25 @@
+## download ./db/production.sqlite3
+unless ENV['SKIP_GOOGLE_CLOUD_STORAGE'] == '1' # skip during assets:precompile
+  require 'google/cloud/storage'
+
+  credentials =
+    if ENV['CREDENTIALS_JSON']
+      JSON.parse(ENV['CREDENTIALS_JSON'])
+    else
+      'devs-sandbox-5941dd8999bb.json'
+    end
+  storage = Google::Cloud::Storage.new(
+    project_id: 'devs-sandbox',
+    credentials: credentials,
+  )
+
+  bucket = storage.bucket('try-rails7')
+  file = bucket.file('production.sqlite3')
+  file.download("db/#{Rails.env}.sqlite3")
+end
+
+## ok
+
 require_relative "boot"
 
 require "rails/all"
